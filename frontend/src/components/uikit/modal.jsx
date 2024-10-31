a8cfef90d16d2d969631268f7f5f7fd863e41cd3
Leonid <144706909+Lonja971@users.noreply.github.com>
2024-10-31 13:05:24 +0100

    Changed "HOME" button.

--- frontend/src/components/uikit/modal.jsx
+++ frontend/src/components/uikit/modal.jsx
@@ -5,9 +5,12 @@ export const Modal = ({ active, children, handleModalOpen }) => {
     <div className={active ? "modal active" : "modal"}>
       <div className="modal__container">
         <div className="fake-nav"></div>
-        <div className="_glass modal-close-block">
-          <div className="modal-close-kruist" onClick={() => handleModalOpen("close")}>
-            HOME
+        <div className="_glass modal-close-block" onClick={() => handleModalOpen("close")}>
+          <div className="modal-close-kruist">
+            <img
+              src="img/servise/home.png"
+              alt="HOME"
+            />
           </div>
         </div>
         {children}
